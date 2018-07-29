71030b709132f564311fe4eb44e4e5810c3d5718
Adam Gajzlerowicz <nelf86@gmail.com>
2018-07-29 09:27:22 +0200

    push

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,30 +2,36 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
-    |> route
-    |> format_response
+    # |> route
+    # |> format_response
   end
 
   def parse(request) do
     # TODO: Parse the request string into a map:
-    %{method: "GET", path: "/wildthings", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
-  def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
-  end
+  # def route(conv) do
+  #   # TODO: Create a new map that also has the response body:
+  #   %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+  # end
 
-  def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
-    """
-    HTTP/1.1 200 OK
-    Content-Type: text/html
-    Content-Length: 20
+  # def format_response(conv) do
+  #   # TODO: Use values in the map to create an HTTP response string:
+  #   """
+  #   HTTP/1.1 200 OK
+  #   Content-Type: text/html
+  #   Content-Length: 20
 
-    Bears, Lions, Tigers
-    """
-  end
+  #   Bears, Lions, Tigers
+  #   """
+  # end
 end
 
 request = """
@@ -33,7 +39,6 @@ GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
-
 """
 
 response = Servy.Handler.handle(request)
