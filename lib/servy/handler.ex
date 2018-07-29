855c05ef28192928236abd5b760f85863e5ae79d
Adam Gajzlerowicz <nelf86@gmail.com>
2018-07-29 22:38:31 +0200

    push

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,44 +2,104 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
-    # |> route
-    # |> format_response
+    |> route
+    |> format_response
   end
 
+  def log(data), do: IO.inspect data
+
   def parse(request) do
-    # TODO: Parse the request string into a map:
     [method, path, _] =
       request
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
-  # def route(conv) do
-  #   # TODO: Create a new map that also has the response body:
-  #   %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
-  # end
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_reason conv.status}
+    Content-Type: text/html
+    Content-Length: #{String.length conv.resp_body}
 
-  # def format_response(conv) do
-  #   # TODO: Use values in the map to create an HTTP response string:
-  #   """
-  #   HTTP/1.1 200 OK
-  #   Content-Type: text/html
-  #   Content-Length: 20
+    #{conv.resp_body}
+    """
+  end
 
-  #   Bears, Lions, Tigers
-  #   """
-  # end
+  def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200 }
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington", status: 200 }
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | resp_body: "Bear #{id}", status: 200 }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | resp_body: "No path #{path} found", status: 404 }
+  end
+
+  defp status_reason(status) do
+    %{
+      200 => :OK,
+      404 => :Dupa
+    }[status]
+  end
+
 end
 
+
+
 request = """
-GET /wildthings HTTP/1.1
+GET /bears/3 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 """
 
-response = Servy.Handler.handle(request)
-IO.puts(response)
+
+IO.puts(Servy.Handler.handle(request))
+
+# request = """
+# GET /wildthings HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+# """
+
+
+# IO.puts(Servy.Handler.handle(request))
+
+# request = """
+# GET /bears HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+# """
+
+
+# IO.puts(Servy.Handler.handle(request))
+
+# request = """
+# GET /dupa HTTP/1.1
+# Host: example.com
+# User-Agent: ExampleBrowser/1.0
+# Accept: */*
+# """
+
+# IO.puts(Servy.Handler.handle(request));
